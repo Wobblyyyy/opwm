fe868f8039da806962696ab618116d1d56fa77b4
colin robertson <30844830+Wobblyyyy@users.noreply.github.com>
2021-05-06 11:32:53 -0400

    Create compression.js

---
+++ src/lib/compression.js
@@ -0,0 +1,9 @@
+const lzw = {
+  // Takes a string input and returns a compressed string output.
+  encode: function (text) {
+  },
+  
+  // Takes a compressed string input and returns a string output.
+  decode: function (encoded) {
+  }
+}
